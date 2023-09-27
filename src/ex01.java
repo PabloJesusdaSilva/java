e93d53b30631c059fa69c447d62c4520970de227
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-27 08:13:49 -0300

    :tada: Commit initial

---
+++ src/ex01.java
@@ -0,0 +1,12 @@
+class ex01 {
+    public static void main (String[] args) {
+        // Create a code that multiplies tree numbers
+
+        int numberOne = 10;
+        int numberTwo = 132;
+        int result = numberOne + numberTwo;
+
+        // Print the result of the result variable
+        System.out.print("O resultado é" + result);
+    }
+}
