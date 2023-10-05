419d53ca9e57b4df2d8d77e1a9f7500577fd4e4c
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-10-05 09:38:14 -0300

    :feat: criando novos exercícios

---
+++ exerciciosJava/src/ex24.java
@@ -0,0 +1,11 @@
+public class ex24 {
+   public static void main(String[] args) {
+      for(int i = 0; i <= 10; i++) {
+         goodMorning();
+      }
+   }
+
+   static void goodMorning() {
+      System.out.print("Hello World!");
+   }
+}
